3b61257e968278b5fccb1c69cc87702f73cc0b2d
meng <meng.liu1@bthome.com>
2023-03-14 20:02:06 +0800

    init

---
+++ components/flow_list/flow_list.js
@@ -0,0 +1,26 @@
+// components/flow_list/flow_list.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        itemList: {
+            type: Array,
+            value: []
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+Page({
+  data: {
+      items: [
+          {name: '', icon: '', mode: 1,tag: 'left'},
+          {name: '', icon: '', mode: 0,tag: 'right'},
+          {name: '', icon: '', mode: 0,tag: 'left'},
+          {name: '', icon: '', mode: 1,tag: 'right'},
+          {name: '', icon: '', mode: 1,tag: 'left'},
+      ]
+  }
+})
